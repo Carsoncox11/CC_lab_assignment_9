aefa62ca223180658ea8d84482d965ba170678f7
Carsoncox11 <125503977+Carsoncox11@users.noreply.github.com>
2023-04-12 17:59:58 -0400

    Implemented lab assignment 9

--- lab9.c
+++ lab9.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// RecordType
 struct RecordType
 {
 	int		id;
 	char	name;
 	int		order; 
+	struct RecordType* next;
 };
 
-// Fill out this structure
 struct HashType
 {
-
+	struct RecordType* record;
 };
 
-// Compute the hash function
-int hash(int x)
+//hashes the data by taking in the ASCII value of the letter name and modulus it by the table size
+int hash(int x, int tableSize)
 {
-
+	return x % tableSize;
 }
 
 // parses input file to an integer array
@@ -69,6 +69,30 @@ void printRecords(struct RecordType pData[], int dataSz)
 	printf("\n\n");
 }
 
+// insert record into the hash table
+void insertRecord(struct HashType *hashTable, struct RecordType *record, int tableSize)
+{
+    // call the hash function to get the index, pass in the ASCII value of the name letter
+	int hashIndex = hash((int) record->name, tableSize);
+
+	//if it is NULL at that index, assign that index to the record
+	if((hashTable + hashIndex)->record == NULL)
+		(hashTable + hashIndex)->record = record;
+	else
+	{
+		//create a temporary pointer for traversal
+		struct RecordType * temp = (hashTable + hashIndex)->record;
+
+		//traverse to the end of the linked list if it is not NULL
+		while(temp->next != NULL)
+			temp = temp->next;
+		
+		//once at the end of the linked list, place the record at the end
+		temp->next = record;
+	}
+}
+
+
 // display records in the hash structure
 // skip the indices which are free
 // the output will be in the format:
@@ -76,10 +100,27 @@ void printRecords(struct RecordType pData[], int dataSz)
 void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
 {
 	int i;
+	struct RecordType * temp;
 
 	for (i=0;i<hashSz;++i)
 	{
-		// if index is occupied with any records, print all
+		// if index is occupied with any records, print all the records
+		if((pHashArray + i)->record != NULL)
+		{
+			//temporary pointer for traversal
+			temp = (pHashArray + i)->record;
+
+			printf("Index %d ", i);
+
+			//while temp is not NULL, print out the record and traverse the linked list
+			while(temp != NULL)
+			{
+				printf("-> %d, %c, %d ", temp->id, temp->name, temp->order);
+				temp = temp->next;
+			}
+
+			printf("-> NULL\n");
+		}
 	}
 }
 
@@ -90,5 +131,23 @@ int main(void)
 
 	recordSz = parseData("input.txt", &pRecords);
 	printRecords(pRecords, recordSz);
-	// Your hash implementation
+
+	//initialize the hash table and the table size
+	struct HashType *hashTable;
+	int hashTableSize = 11;
+
+	//allocate space for the hash table, using calloc to set all to NULL
+	hashTable = calloc(hashTableSize, sizeof(struct HashType));
+
+	//insert each record into the hash table
+	for(int i = 0; i < recordSz; i++)
+		insertRecord(hashTable, pRecords + i, hashTableSize);
+	
+	displayRecordsInHash(hashTable, hashTableSize);
+
+	//free the allocated memory
+	free(hashTable);
+	free(pRecords);
+
+	return 0;
 }
